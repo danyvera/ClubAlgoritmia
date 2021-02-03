55273bad9a7be24fb6854035621f81196a84495e
Daniela Vera <54813226+danyvera@users.noreply.github.com>
2021-02-02 18:15:08 -0600

    Create Social Network (easy version).cpp

---
+++ Basicos/SegundoContest/Social Network (easy version).cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define    mii        map <int,int>
+
+
+int main(){
+  int n,k,val,i,x,p,len;
+
+    while (sf ("%d %d",&n,&k) != EOF){
+        mii mp;
+        stack <int> st;
+
+        for (i=1; i<=n; i++){
+            cin>>val;
+
+            if (st.empty()){
+                mp[val] = 1;
+                st.push(val);
+            }
+            else {
+                p = st.size()-mp[val]+1;
+
+                if (mp[val] == 0 || (p > 0 && p > k)) {
+                    x = st.top();
+                    mp[val] = mp[x]+1;
+                    st.push(val);
+                }
+            }
+        }
+
+        if (st.size() > k)
+            len = k;
+        else
+            len = st.size();
+
+        cout<<len<<st.top());
+        st.pop();
+        --len;
+
+        while (!st.empty()) {
+            if (!len)
+                break;
+
+            cout<<st.top();
+            --len;
+            st.pop();
+        }
+
+        cout<<"\n";
+    }
+  }
